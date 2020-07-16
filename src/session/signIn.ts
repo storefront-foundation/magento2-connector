710b1137237ecfcdc69c4bfbdb34684095827316
Mark Brocato <mark.brocato@moovweb.com>
2020-07-16 23:12:15 +0300

    Fix error handling.

--- src/session/signIn.ts
+++ src/session/signIn.ts
@@ -17,51 +17,43 @@ export default async function signIn(
   req: Request,
   res: Response
 ): Promise<Session> {
-  try {
-    const { token } = await fetchGenerateToken({ email, password })
+  const { token } = await fetchGenerateToken({ email, password })
 
-    if (!token) {
-      // unsuccessful login
-      throw new Error(
-        'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.'
-      )
-    }
+  if (!token) {
+    // unsuccessful login
+    throw new Error(
+      'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.'
+    )
+  }
 
-    // fetch customer cart data
-    const customerCartData = await fetchCustomerCart(token)
-    let { cart, customerCartId } = customerCartData
+  // fetch customer cart data
+  const customerCartData = await fetchCustomerCart(token)
+  let { cart, customerCartId } = customerCartData
 
-    // if guest has cart items -> merge it with logged in customer cart
-    const guestCartIdCookieValue = getCookieValue(req, COOKIES.M2_GUEST_CART_ID)
-    if (guestCartIdCookieValue) {
-      const sourceCartId = guestCartIdCookieValue
-      const destinationCartId = customerCartId
-      const mergeCartsData = await fetchMergeCarts({ token, sourceCartId, destinationCartId })
-      cart = get(mergeCartsData, 'cart', cart)
-      customerCartId = get(mergeCartsData, 'customerCartId', customerCartId)
-    }
+  // if guest has cart items -> merge it with logged in customer cart
+  const guestCartIdCookieValue = getCookieValue(req, COOKIES.M2_GUEST_CART_ID)
+  if (guestCartIdCookieValue) {
+    const sourceCartId = guestCartIdCookieValue
+    const destinationCartId = customerCartId
+    const mergeCartsData = await fetchMergeCarts({ token, sourceCartId, destinationCartId })
+    cart = get(mergeCartsData, 'cart', cart)
+    customerCartId = get(mergeCartsData, 'customerCartId', customerCartId)
+  }
 
-    // @TODO: also customer account data like firstName, lastName can be fetched here
-    // Docs: https://devdocs.magento.com/guides/v2.3/graphql/queries/customer.html
-    // ...
+  // @TODO: also customer account data like firstName, lastName can be fetched here
+  // Docs: https://devdocs.magento.com/guides/v2.3/graphql/queries/customer.html
+  // ...
 
-    const cookiesToSet = []
-    cookiesToSet.push(
-      prepareSetCookie(COOKIES.M2_CUSTOMER_TOKEN, token, { maxAge: 3600 * 24 * 30 })
-    ) // set customer token cookie for 30 days
-    cookiesToSet.push(
-      prepareSetCookie(COOKIES.M2_CUSTOMER_CART_ID, customerCartId, { maxAge: 3600 * 24 * 30 })
-    ) // set customer cart ID cookie for 30 days
-    cookiesToSet.push(prepareKillCookie(COOKIES.M2_GUEST_CART_ID)) // kill guest cart ID cookie just in case (prevents possible cart merges issues)
-    setCookies(res, cookiesToSet)
+  const cookiesToSet = []
+  cookiesToSet.push(prepareSetCookie(COOKIES.M2_CUSTOMER_TOKEN, token, { maxAge: 3600 * 24 * 30 })) // set customer token cookie for 30 days
+  cookiesToSet.push(
+    prepareSetCookie(COOKIES.M2_CUSTOMER_CART_ID, customerCartId, { maxAge: 3600 * 24 * 30 })
+  ) // set customer cart ID cookie for 30 days
+  cookiesToSet.push(prepareKillCookie(COOKIES.M2_GUEST_CART_ID)) // kill guest cart ID cookie just in case (prevents possible cart merges issues)
+  setCookies(res, cookiesToSet)
 
-    return {
-      cart,
-      signedIn: true,
-    }
-  } catch (error) {
-    return res.status(400).send({
-      error: get(error, 'message', 'An error occurred during sign in'),
-    })
+  return {
+    cart,
+    signedIn: true,
   }
 }
