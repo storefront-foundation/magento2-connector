9df07ee7f81cfb6597004e0c57db4275d3e43a3b
Egor Mesyats <egor.mesyats@moovweb.com>
2020-09-22 15:53:39 +0300

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript/base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'max-len': ['error', { 'code': 120 }],
+    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+  },
+};
